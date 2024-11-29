23d55406d07b5e79c8a25659374e35749fa5a1ad
Termi007xt <sharmamaulik95@gmail.com>
2024-11-29 21:09:28 +0530

    contact form functional

--- OURS/HOMEPAGE.php
+++ OURS/HOMEPAGE.php
@@ -338,3 +338,22 @@ if (!isset($_SESSION["user"])) {
     <p>Copyright ©️ Smart</p>
   </div>
 </div>
+
+<script>
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbxemBWtQzTek37AMtQhQI-qJ60Mp9_HdQ2kMNzIu1fvnnnYwRxCPw638_Ty2jYXKEXr3A/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => 
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+          msg.innerHTML = ""
+        },3000)
+        form.reset()
+      )
+      .catch(error => console.error('Error!', error.message))
+  })
+</script>
